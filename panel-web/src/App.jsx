68b91249f4ad81136013ffe7bb3e9a5b38a29b14
ValcocerLopez <vmalcocer@uamv.edu.ni>
2026-07-24 10:45:04 -0600

    feat: detalle de respuestas por alumno

--- panel-web/src/App.jsx
+++ panel-web/src/App.jsx
@@ -3,6 +3,7 @@ import { AuthProvider, useAuth } from './context/AuthContext.jsx'
 import Login from './views/Login.jsx'
 import Dashboard from './views/Dashboard.jsx'
 import SalaDetalle from './views/SalaDetalle.jsx'
+import AlumnoDetalle from './views/AlumnoDetalle.jsx'
 import Preguntas from './views/Preguntas.jsx'
 
 function ProtectedRoute({ children }) {
@@ -30,6 +31,9 @@ export default function App() {
           } />
           <Route path="/salas/:id" element={
             <ProtectedRoute><SalaDetalle /></ProtectedRoute>
+          } />
+          <Route path="/salas/:id/alumnos/:studentId" element={
+            <ProtectedRoute><AlumnoDetalle /></ProtectedRoute>
           } />
           <Route path="/preguntas" element={
             <ProtectedRoute><Preguntas /></ProtectedRoute>
