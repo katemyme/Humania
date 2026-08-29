23d85966938fad94d7fbad34f56ab2bd6f99588c
Mauro Delgado <maurods@uamv.edu.ni>
2026-08-29 02:40:36 -0600

    feat: agregar input fields para el login

---
+++ juego-unity/Assets/Scripts/get_text.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using TMPro;
+
+public class TMPInputFieldPlaceholderFix : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+
+    private void Start()
+    {
+
+    }
+}
